e4b6b1d2bde9d9b50dff6e327e99b80a9dfbf8f7
nathrizandi <nathasya.rizandi@gmail.com>
2024-07-15 14:27:26 +0700

    .

--- resources/views/course/detail.blade.php
+++ resources/views/course/detail.blade.php
@@ -51,7 +51,6 @@
                 <div class="accordion" id="accordionExample">
                     <div class="course-week-content" data-content="week-1">
                         @for ($i = 1; $i <= 4; $i++)
-                            <a href="/course/media" style="text-decoration: none; color: inherit; display: block">
                                 <div class="accordion-item">
                                     <h2 class="accordion-header" id="heading{{ $i }}">
                                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
@@ -62,14 +61,15 @@
                                     </h2>
                                     <div id="collapse{{ $i }}" class="accordion-collapse collapse"
                                         aria-labelledby="heading{{ $i }}" data-bs-parent="#accordionExample">
-                                        @for ($j = 1; $j <= 5; $j++)
-                                            <div class="accordion-body">
-                                                Week {{ $i }} - Sub title {{ $j }}
-                                            </div>
-                                        @endfor
+                                        <a href="/course/media" style="text-decoration: none; color: inherit; display: block">
+                                            @for ($j = 1; $j <= 5; $j++)
+                                                <div class="accordion-body">
+                                                    Week {{ $i }} - Sub title {{ $j }}
+                                                </div>
+                                            @endfor
+                                        </a>
                                     </div>
                                 </div>
-                            </a>
                         @endfor
                     </div>
 
--- resources/views/course/media.blade.php
+++ resources/views/course/media.blade.php
@@ -2,10 +2,26 @@
 
 @section('title', 'Course')
 
-{{-- @section('custom-csspage', '/css/course-view.css') --}}
+@section('custom-csspage', '/css/course-media.css')
 
 @section('content')
     <div class="row">
-        
+        <a href="/course/detail" style="text-decoration: none; color: inherit; display: block">
+            <img src="https://github.com/nathrizandi/brainery/blob/main/public/assets/icon/back.png?raw=true" style="width: 3.5vw">
+        </a>
+    </div>
+    <div class="video-container mt-2" style="margin-left: 2%">
+        <div class="row">
+            <iframe width="1691" height="622" src="https://www.youtube.com/embed/R9PTBwOzceo?list=PLBlnK6fEyqRj9lld8sWIUNwlKfdUoPd1Y" title="Introduction to Linked List" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    </div>
+    <div class="row mt-5" style="margin-left: 2%">
+        <div class="col-10">
+            <h1 class="text-orange">Title</h1>
+            <p>description fdkgklgklgmkldgmdkdgmggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</p>
+        </div>
+        <div class="col-2">
+            <img src="https://github.com/nathrizandi/brainery/blob/main/public/assets/brain8.png?raw=true" style="width: 8vw">
+        </div>
     </div>
 @endsection
