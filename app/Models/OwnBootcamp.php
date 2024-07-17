467a92f1b9316dce6e83eb8cd0d7d10dc2031ba5
Joeliardo <jleviothniel@gmail.com>
2024-07-17 11:33:34 +0700

    seederdilanjutinmetiu

--- app/Models/OwnBootcamp.php
+++ app/Models/OwnBootcamp.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class OwnBootcamp extends Model
 {
     use HasFactory;
+    protected $table = "own_bootcamps";
+    protected $guarded = [];
 }
--- database/seeders/CourseMaterialSeeder.php
+++ database/seeders/CourseMaterialSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Course;
+use App\Models\CourseMaterial;
 
 class CourseMaterialSeeder extends Seeder
 {
@@ -12,6 +15,17 @@ class CourseMaterialSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create('id_ID');
+        $courses = Course::all();
+        $weeks = ["Week 1", "Week 2", "Week 3", "Week 4"];
+        foreach ($courses as $course) {
+            foreach ($weeks as $week) {
+                CourseMaterial::create([
+                    "course_id"=>$course->id,
+                    "title"=>$week,
+                ]);                
+            }
+            
+        }
     }
 }
