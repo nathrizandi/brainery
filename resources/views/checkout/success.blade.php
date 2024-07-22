71550a2ff3f79b50616e8b9b0366d032ee1292e5
Matthew <matthew.setiawan002@binus.ac.id>
2024-07-22 14:48:36 +0700

    Midtrans kelar

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -8,10 +8,13 @@
     </div>
     <div class="row justify-content-center text-center">
         <h1 class="text-orange">Payment Successful</h1>
-        <h3 style="font-weight: 550; margin-top: 1%">Hooray! You have completed your payment. Let's explore all the courses and enhance your knowledge and skills.</h3>
+        <h3 style="font-weight: 550; margin-top: 1%">Hooray! You have completed your payment. Let's explore all the courses
+            and enhance your knowledge and skills.</h3>
     </div>
 
     <div class="row justify-content-center text-center mt-4">
-        <a href="#" type="button" class="btn" style="width: 25%; height: 15%; font-weight: bold; background-color: #F76D3B; color: white">View Course List</a>
+        <a href="#" type="button" class="btn"
+            style="width: 25%; height: 15%; font-weight: bold; background-color: #F76D3B; color: white">View Course List</a>
     </div>
+
 @endsection
