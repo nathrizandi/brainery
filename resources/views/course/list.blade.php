08322de4070b1480bbb3873a21670b97f8a4eb88
gsellz <grisella.zega@binus.ac.id>
2024-07-23 14:28:15 +0700

    list course show more

---
+++ resources/views/course/list.blade.php
@@ -0,0 +1,26 @@
+@extends('/template/layout')
+
+@section('title', 'View Course')
+
+@section('content')
+    <h1 class="text-orange">Course List</h1>
+    <h3>All free bootcamps you can register to</h3>
+
+    <div class="row">
+        @foreach ($courseList as $item)
+            <div class="col-3 mt-2">
+                <a href="{{ route('courseView', $item->id) }}"
+                    style="text-decoration: none; color: inherit; display: block">
+                    <div class="card shadow-sm mb-3" style="height: 45vh">
+                        <img src="{{ asset($item->courseImage) }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title">{{ $item->title }}</h3>
+                            <p class="card-writer">by: {{ $item->nama }}</p>
+                            <p class="card-description">{{ $item->description }}</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+@endsection
